da5b3ef42a12364d6a5ad4a3846e8b513d1d3683
hyeonjun kim <k656000@gmail.com>
2023-01-29 22:26:34 +0900

    create customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-        <img src={logo} lat="logo"/>
-        <h2>Let's go!!</h2>
-    </div>
-  );
+import './App.css';
+import Customer from './components/customer';
+import { render } from '@testing-library/react';
+import { Component } from 'react';
+
+const customers =[
+  {
+  id : "1",
+  image : "http://placeimg.com/64/64/1",
+  name : "홍길동",
+  birthday : "000525",
+  gender : "남자",
+  job : "학생",
+  },
+  {
+    id : "2",
+    image : "http://placeimg.com/64/64/2",
+    name : "이순신",
+    birthday : "010525",
+    gender : "남자",
+    job : "개발자",
+    },
+    {
+      id : "3",
+      image : "http://placeimg.com/64/64/3",
+      name : "이재용",
+      birthday : "000215",
+      gender : "남자",
+      job : "백수",
+      },
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+          customers.map(c=>{
+            return (
+              <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+              />
+            )
+          })
+        }
+        
+        
+      </div>
+    );
+  }
 }
 
 export default App;
